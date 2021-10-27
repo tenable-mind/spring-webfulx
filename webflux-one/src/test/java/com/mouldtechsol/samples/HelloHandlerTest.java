07b95afbb7592ef35d19c95b6e9e3e25b15e6bbb
Mind Cape <mindcape@gmail.com>
2021-10-26 21:21:32 -0400

    initial commit

---
+++ webflux-one/src/test/java/com/mouldtechsol/samples/HelloHandlerTest.java
@@ -0,0 +1,24 @@
+package com.mouldtechsol.samples;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+public class HelloHandlerTest {
+	private WebTestClient testClient;
+
+	@BeforeAll
+	void setUp() throws Exception {
+		this.testClient = WebTestClient.bindToRouterFunction(new HelloHandler().routes())
+				.build();
+	}
+
+	@Test
+	void testHello() throws Exception {
+		this.testClient.get().uri("/") //
+				.exchange() //
+				.expectStatus().isOk() //
+				.expectBody(String.class).isEqualTo("Hello World!");
+	}
+}
---
+++ webflux-one/src/test/java/com/mouldtechsol/samples/MessageHandlerTest.java
@@ -0,0 +1,42 @@
+package com.mouldtechsol.samples;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.hamcrest.Matchers.is;
+
+public class MessageHandlerTest {
+	private WebTestClient testClient;
+
+	@BeforeAll
+	void setUp() throws Exception {
+		this.testClient = WebTestClient
+				.bindToRouterFunction(new MessageHandler().routes()).build();
+	}
+
+	@Test
+	void testInvalidMessage() throws Exception {
+		this.testClient.post().uri("/messages") //
+				.bodyValue(new Message("HelloHello")).exchange() //
+				.expectStatus().isBadRequest() //
+				.expectBody(JsonNode.class)
+				.value(n -> n.get("details").get(0).get("defaultMessage").asText(), is(
+						"The size of \"text\" must be less than or equal to 8. The given size is 10"));
+	}
+
+	@Test
+	void testMessage() throws Exception {
+		this.testClient.post().uri("/messages") //
+				.bodyValue(new Message("Hello")).exchange() //
+				.expectStatus().isOk() //
+				.expectBody(String.class).isEqualTo("{\"text\":\"Hello\"}");
+
+		this.testClient.get().uri("/messages") //
+				.exchange() //
+				.expectStatus().isOk() //
+				.expectBody(String.class).isEqualTo("[{\"text\":\"Hello\"}]");
+	}
+}
